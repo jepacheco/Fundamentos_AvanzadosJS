d84bf8ebb98d1bcd5c0ff29863b146bf3e68cf48
Jesus Pacheco <je.pacheco2311@gmail.com>
2020-11-24 16:24:37 +0100

    Callback hell

--- callbacks.js
+++ callbacks.js
@@ -2,21 +2,6 @@ const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
 var characters = []
-const onPeopleResponse = function(character) {
-    characters.push(character.name)
-    console.log(`Hola, yo soy ${character.name}`);
-}
-
-function getCharacter(id) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, opts, onPeopleResponse)
-}
-
-// Al usar un metodo asincrono dependera de la respuesta del servidor
-// aunque se coloque un orden no sabremos cual respondera primero.
-//getCharacter(1)
-//getCharacter(2)
-//getCharacter(3)
 
 function fillAnShowArrayCharacters() {
     for (let i = 1; i <= 10; i++) {
@@ -25,4 +10,31 @@ function fillAnShowArrayCharacters() {
     }
 }
 
-fillAnShowArrayCharacters()
+function getCharacter(id, callback) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+    $.get(url, opts, function(character) {
+        console.log(`Hola, yo soy ${character.name}`);
+
+        if (callback) {
+            callback()
+        }
+    })
+}
+
+getCharacter(1, function() {
+        getCharacter(2, function() {
+            getCharacter(3, function() {
+                getCharacter(4, function() {
+                    getCharacter(5, function() {
+                        getCharacter(6)
+                    })
+                })
+            })
+        })
+    })
+    // Al usar un metodo asincrono dependera de la respuesta del servidor
+    // aunque se coloque un orden no sabremos cual respondera primero.
+    //getCharacter(1)
+    //getCharacter(2)
+    //getCharacter(3)
