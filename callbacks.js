838ab6240c289d530625f185a772909ef889bbce
Jesus Pacheco <je.pacheco2311@gmail.com>
2020-11-24 17:20:42 +0100

    Uso de promesas para sustituir el callback

--- callbacks.js
+++ callbacks.js
@@ -1,49 +1,31 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
-var characters = []
 
-function fillAnShowArrayCharacters() {
-    for (let i = 1; i <= 10; i++) {
-        const id = i
-        getCharacter(id)
-    }
-}
+function getCharacter(id) {
 
-function getCharacter(id, callback) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-
-    $.get(url, opts, callback).fail(() => {
-        console.log(`Sucedio un error no se encontro el personaje id`);
-    })
-
-}
-
-getCharacter(1, function(character) {
-        console.log(`Hola, yo soy ${character.name}`)
-
-        getCharacter(2, function(character) {
-            console.log(`Hola, yo soy ${character.name}`)
-
-            getCharacter(3, function(character) {
-                console.log(`Hola, yo soy ${character.name}`)
-
-                getCharacter(4, function(character) {
-                    console.log(`Hola, yo soy ${character.name}`)
-
-                    getCharacter(5, function(character) {
-                        console.log(`Hola, yo soy ${character.name}`)
-
-                        getCharacter(6, function(character) {
-                            console.log(`Hola, yo soy ${character.name}`)
-                        })
-                    })
-                })
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $
+            .get(url, opts, function(data) {
+                resolve(data)
             })
-        })
+            .fail(() => reject(id))
     })
-    // Al usar un metodo asincrono dependera de la respuesta del servidor
-    // aunque se coloque un orden no sabremos cual respondera primero.
-    //getCharacter(1)
-    //getCharacter(2)
-    //getCharacter(3)
+}
+
+function onError(id) {
+    console.log(`Sucedio un error al obtener el personaje ${id}`);
+}
+
+getCharacter(1)
+    .then(function(character) {
+        console.log(`El personaje es ${character.name}`)
+    })
+    .catch(onError)
+
+// Al usar un metodo asincrono dependera de la respuesta del servidor
+// aunque se coloque un orden no sabremos cual respondera primero.
+//getCharacter(1)
+//getCharacter(2)
+//getCharacter(3)
