288433f08e3b88c72fc2ecccf9153fd7981d7329
Jesus Pacheco <je.pacheco2311@gmail.com>
2020-11-29 18:03:47 +0100

    estructuras de datos inmutables se crea una nueva variable para no modificar el original

--- estructuraDtosInmutables.js
+++ estructuraDtosInmutables.js
@@ -1,10 +1,14 @@
 const jesus = {
     nombre: 'Jesus',
     apellido: 'Pacheco',
-    edad: 28
+    edad: 27
 }
 
 const cumpleanosInmutable = persona => ({
     ...persona,
     edad: persona.edad + 1
 })
+
+//Se crea un nuevo objeto donde se guarrdara la nueva edad
+//sin alterar el original
+const jesus28 = cumpleanosInmutable(jesus)
