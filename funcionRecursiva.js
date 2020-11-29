58e6952494530f4124364bf1d101568f93d18760
Jesus Pacheco <je.pacheco2311@gmail.com>
2020-11-29 17:27:29 +0100

    Funcion recursiva utilizando cache

--- funcionRecursiva.js
+++ funcionRecursiva.js
@@ -5,3 +5,24 @@ function divisionEntera(dividendo, divisor) {
 
     return 1 + divisionEntera(dividendo - divisor, divisor)
 }
+//---------------------------------------------------------------
+
+function factorial(n) {
+
+    if (!this.cache) {
+        //Se crea el objeto cache para guardar resultados
+        this.cache = {}
+    }
+
+    if (this.cache[n]) {
+        return this.cache[n]
+    }
+
+    if (n === 1) {
+        return 1
+    }
+
+    //Se llena el objeto cache con el resultado obtenido
+    this.cache[n] = n * factorial(n - 1)
+    return this.cache[n]
+}
